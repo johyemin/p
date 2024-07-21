baf1494cffef0db01159643484d49bb5c760542b
johyemin <49671771+johyemin@users.noreply.github.com>
2024-07-21 21:14:46 +0900

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,62 @@
+$(function(){
+    
+    $('.html_bar').easyPieChart({
+        barColor: '#FFC314',
+        trackColor: '#ccc',
+        scaleColor: '#fff',
+        lineCap: 'round',
+        lineWidth: 5,
+        size: 60,
+        animate: 1000,
+        onStart: $.noop,
+        onStop: $.noop
+    });
+
+    $('.css_bar').easyPieChart({
+        barColor: '#FFC314',
+        trackColor: '#fff',
+        scaleColor: '#ccc',
+        lineCap: 'round',
+        lineWidth: 5,
+        size: 60,
+        animate: 1000,
+        onStart: $.noop,
+        onStop: $.noop
+    });
+
+    $('.js_bar').easyPieChart({
+        barColor: '#FFC314',
+        trackColor: '#fff',
+        scaleColor: '#fff',
+        lineCap: 'round',
+        lineWidth:5,
+        size: 60,
+        animate: 1000,
+        onStart: $.noop,
+        onStop: $.noop
+    });
+
+    $('.photoshop_bar').easyPieChart({
+        barColor: '#FFC314',
+        trackColor: '#fff',
+        scaleColor: '#ccc',
+        lineCap: 'round',
+        lineWidth: 5,
+        size: 60,
+        animate: 1000,
+        onStart: $.noop,
+        onStop: $.noop
+    });
+
+    $('.illust_bar').easyPieChart({
+        barColor: '#FFC314',
+        trackColor: '#fff',
+        scaleColor: '#ccc',
+        lineCap: 'round',
+        lineWidth: 5,
+        size: 60,
+        animate: 1000,
+        onStart: $.noop,
+        onStop: $.noop
+    });
+})
